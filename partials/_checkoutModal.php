9415885f427ef7b9d74a4ce15f4b810a3525ebf2
SyahRizwan2603 <134351789+SyahRizwan2603@users.noreply.github.com>
2023-07-05 11:37:24 +0800

    Update _checkoutModal.php

--- partials/_checkoutModal.php
+++ partials/_checkoutModal.php
@@ -12,25 +12,25 @@
             <form action="partials/_manageCart.php" method="post">
                 <div class="form-group">
                     <b><label for="address">Address:</label></b>
-                    <input class="form-control" id="address" name="address" placeholder="1234 Main St" type="text" required minlength="3" maxlength="500">
+                    <input class="form-control" id="address" name="address" placeholder="Enter your address" type="text" required minlength="3" maxlength="500">
                 </div>
                 <div class="form-group">
                     <b><label for="address1">Address Line 2:</label></b>
-                    <input class="form-control" id="address1" name="address1" placeholder="near st, Surat, Gujarat" type="text">
+                    <input class="form-control" id="address1" name="address1" placeholder="Enter your address 2" type="text">
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6 mb-0">
                         <b><label for="phone">Phone No:</label></b>
                         <div class="input-group mb-3">
                         <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon">+91</span>
+                            <span class="input-group-text" id="basic-addon">+60</span>
                         </div>
                         <input type="tel" class="form-control" id="phone" name="phone" placeholder="xxxxxxxxxx" required pattern="[0-9]{10}" maxlength="10">
                         </div>
                     </div>
                     <div class="form-group col-md-6 mb-0">
-                        <b><label for="zipcode">Zip Code:</label></b>
-                        <input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="xxxxxx" required pattern="[0-9]{6}" maxlength="6">                    
+                        <b><label for="zipcode">Postcode:</label></b>
+                        <input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="xxxxxx" required pattern="[0-9]{5}" maxlength="6">                    
                     </div>
                 </div>
                 <div class="form-group">
